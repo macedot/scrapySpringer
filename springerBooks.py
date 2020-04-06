86f56a84048782036a2fa3d59da3651fb4ff7000
Thiago Macedo <1750447+macedot@users.noreply.github.com>
2020-04-06 00:06:30 -0300

    v1

--- springerBooks.py
+++ springerBooks.py
@@ -3,6 +3,8 @@ import scrapy
 import requests
 import csv
 import html2text
+import os
+from pathlib import Path
 from collections import defaultdict
 from typing import Dict, List
 
@@ -29,21 +31,18 @@ class BookSpider(scrapy.Spider):
 
     def start_requests(self):
         self.books = read_csv('data/FreeEnglishTextbooks.csv')
-        urls = self.books['OpenURL'][:5]
+        urls = self.books['OpenURL'][:3]
         for idx in range(len(urls)):
             self.log('url = %s' % urls[idx])
             yield scrapy.Request(url=urls[idx],
                                  callback=self.parse,
                                  cb_kwargs={'idx': idx})
 
-    def parse(self, response):
-        idx = response.cb_kwargs['idx']
+    def parse(self, response, idx):
         url = response.url
         self.logger.info("Parse ({})".format(url))
 
         title = "INVALID"
-        authors = "INVALID"
-
         page_title = response.css('.page-title')[0]
         if page_title:
             title = page_title.css('h2 ::text').get()
@@ -51,37 +50,53 @@ class BookSpider(scrapy.Spider):
                 title = page_title.css('h1 ::text').get()
                 title = title.replace(":","-")
 
+        authors = "INVALID"
         list_authors = list()
         for book_authors in response.css('.authors__name'):
             this_author = book_authors.css('::text').get()
             this_author = converter.handle(this_author)
             this_author = this_author.strip("\n")
             list_authors.append(this_author)
-        authors = ",".join(list_authors)
+        if len(list_authors) > 0:
+            authors = ",".join(list_authors)
 
-        pdf = "{}_{}.pdf".format(title, authors)
+        url_pdf = ""
+        for book_authors in response.css('.cta-button-container__item'):
+            for href in book_authors.css('a ::attr(href)'):
+                url_pdf = href.get()
+
+        url_pdf = "https://link.springer.com/" + url_pdf
+        pdf_size = 0
+        pdf_file_name = "{}_{}.pdf".format(title, authors)
+
+        books_path = Path("books")
+        books_temp = Path("temp")
+        pdf_tmp = books_temp / pdf_file_name
+        pdf_final = books_path / pdf_file_name
+
+        if os.path.exists(pdf_final):
+            self.logger.debug("Skipping PDF: ({} -> {})".format(url_pdf, pdf_final))
+            pass
+
+        try:
+            with requests.get(url_pdf) as pdf_file:
+                pdf_size = len(pdf_file.content)
+                with open(pdf_tmp, 'wb') as file:
+                    file.write(pdf_file.content)
+                os.rename(pdf_tmp, pdf_final)
+        except IOError:
+            self.logger.error("Unable to save PDF: ({} -> {})".format(url_pdf, pdf_tmp))
+            pass
+
         yield {
             'idx': idx,
             'title': title,
             'authors': authors,
             'url': url,
-            'pdf': pdf,
+            'url_pdf': url_pdf,
+            'pdf': pdf_final,
+            'pdf_size': pdf_size,
         }
-
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
-
-        # for title in response.css('.post-header>h2'):
-        #     yield {'title': title.css('a ::text').get()}
-
-        # for next_page in response.css('a.next-posts-link'):
-        #     yield response.follow(next_page, self.parse)
-
-        # url = 'https://www.python.org/static/img/python-logo@2x.png'
-        # myfile = requests.get(url)
-        # open('books/PythonImage.png', 'wb').write(myfile.content)
-
 
 if __name__ == '__main__':
     print(scrapy.__version__)
