3f1287082d524e98860e4722d592166e8a92eef1
Thiago Macedo <1750447+macedot@users.noreply.github.com>
2020-04-06 09:34:03 -0300

    Use base information instead of parsing from web page

--- springerBooks.py
+++ springerBooks.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import csv
 import os
+import re
+import csv
+
 from pathlib import Path
-from collections import defaultdict
 from typing import Dict
+from urllib.parse import urlparse
+from collections import defaultdict
 
 import requests
 import scrapy
@@ -11,7 +14,6 @@ import html2text
 
 converter = html2text.HTML2Text()
 converter.ignore_links = True
-
 
 def read_csv(input_file: str, delimiter: str = ',') -> Dict[str, list]:
     """Read file to dict with header as dict keys;"""
@@ -31,10 +33,12 @@ def read_csv(input_file: str, delimiter: str = ',') -> Dict[str, list]:
 class BookSpider(scrapy.Spider):
     name = 'springerBooks'
     books = defaultdict(list)
+    books_path = Path("books")
+    temp_path = Path("temp")
 
     def start_requests(self):
         self.books = read_csv('data/FreeEnglishTextbooks.csv')
-        urls = self.books['OpenURL']
+        urls = self.books['OpenURL'][:1]
         for idx in range(len(urls)):
             self.log('url = %s' % urls[idx])
             yield scrapy.Request(url=urls[idx],
@@ -43,50 +47,49 @@ class BookSpider(scrapy.Spider):
 
     def parse(self, response, idx):
         url = response.url
-        self.logger.info("Parse ({})".format(url))
+        try:
+            self.logger.info("Parse ({})".format(url))
 
-        title = "INVALID"
-        page_title = response.css('.page-title')[0]
-        if page_title:
-            title = page_title.css('h2 ::text').get()
-            if title is None:
-                title = page_title.css('h1 ::text').get()
-                title = title.replace(":", "-")
+            title = self.books['Book Title'][idx]
+            authors = self.books['Author'][idx]
+            parsed_uri = urlparse(response.url)
 
-        authors = "INVALID"
-        list_authors = list()
-        for book_authors in response.css('.authors__name'):
-            this_author = book_authors.css('::text').get()
-            this_author = converter.handle(this_author)
-            this_author = this_author.strip("\n")
-            list_authors.append(this_author)
-        if len(list_authors) > 0:
-            authors = ",".join(list_authors)
+            url_book = ""
+            for book_authors in response.css('.cta-button-container__item'):
+                with book_authors.css('a ::attr(href)') as href:
+                    url_book = href[0].get()
 
-        url_book = ""
-        for book_authors in response.css('.cta-button-container__item'):
-            for href in book_authors.css('a ::attr(href)'):
-                url_book = href.get()
+            if len(url_book) == 0:
+                self.logger.error(
+                    "Unable to find Book URL at '{}}'".format(url))
+                return
 
-        url_book = "https://link.springer.com/" + url_book
-        book_size = 0
-        book_file_name = "{}_{}.pdf".format(title, authors)
+            base_uri = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
+            url_book = base_uri + url_book
 
-        books_path = Path("books")
-        books_temp = Path("temp")
-        book_tmp = books_temp / book_file_name
-        book_final = books_path / book_file_name
-
-        if os.path.exists(book_final):
-            self.logger.debug(
-                "Skipping Book: ({} -> {})".format(url_book, book_final))
+            # if os.path.exists(book_final):
+            #     self.logger.debug(
+            #         "Skipping Book: ({}, {} -> {})".format(idx, url_book, book_final))
+            #     return
+        except:
+            self.logger.error(
+                "Error while reading Book URL: ({}, {})'".format(idx, url))
             return
 
         try:
-            with requests.get(url_book) as book_file:
+            with requests.get(url_book) as book_request:
+                if book_file.status_code != 200:
+                    return
+                content_disposition = book_request.headers['content-disposition']
                 book_size = len(book_file.content)
+                if book_size <= 0:
+                    return
+                fname = re.findall("filename=(.+)", content_disposition)[0]
+                book_tmp = self.temp_path / fname
                 with open(book_tmp, 'wb') as file:
                     file.write(book_file.content)
+                book_fname = "{}_{}_{}".format(title, authors, fname)
+                book_final = self.books_path / book_fname
                 os.rename(book_tmp, book_final)
         except IOError:
             self.logger.error(
